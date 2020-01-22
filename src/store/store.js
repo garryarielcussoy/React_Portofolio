15e88abb2517dadb9aabc87a21f5be8639028b4f
Garry Ariel <garry@alterra.id>
2020-01-22 20:53:07 +0700

    Clean code store

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,9 @@ import { stat } from 'fs';
 import { func } from 'prop-types';
 
 const inisialization = {
+    // Search and filter
+    isSearch: false,
+    isFilter: false,
     judulBuku: '',
     namaUserPenjual: '',
     penerbit: '',
@@ -14,8 +17,8 @@ const inisialization = {
     daftarBuku: [],
     category: 'Umum',
     activeCategory: '',
-    isSearch: false,
-    isFilter: false,
+
+    // Book Detail Page
     dataDetilPenjual: {
         username: '',
         nomorHp: '',
@@ -35,6 +38,8 @@ const inisialization = {
         deskripsiBuku: '',
         fotoBuku: ''
     },
+
+    // Register page
     emailRegister: '',
     usernameRegister: '',
     passwordRegister: '',
@@ -44,10 +49,14 @@ const inisialization = {
     nomorHpRegister: '',
     fotoProfilRegister: '',
     isValid: false,
+
+    // Login page
     isLogin: false,
     usernameLogin: '',
     passwordLogin: '',
     tokenLogin: '',
+    
+    // Cart related
     canAdd: '',
     placeholderStart: 0,
     cartList: [],
@@ -56,6 +65,9 @@ const inisialization = {
     usernamePenjualCart: '',
     alamatCart: '',
     nomorHpCart: '',
+
+    // ----- Profile page -----
+    // Profile part
     profileUserData: {
         alamat: '',
         email: '',
@@ -64,12 +76,17 @@ const inisialization = {
         nomorHp: '',
         username: ''
     },
-    bookList: [],
-    notificationList: [],
-    historyList: [],
-    showNotifications: false,
+    // Books list in profile
     showBookList: false,
+    bookList: [],
+    // Notifications list in profile
+    showNotifications: false,
+    notificationList: [],
+    // Shopping histories list
     showHistory: false,
+    historyList: [],
+    
+    // Adding a book
     judulBukuAdd: '',
     pengarangAdd: '',
     penerbitAdd: '',
@@ -79,6 +96,9 @@ const inisialization = {
     hargaSatuanAdd: '',
     categoryAdd: 'Umum',
     deskripsiBukuAdd: '',
+    successAddBook: true,
+
+    // Edit a book
     judulBukuEdit: '',
     pengarangEdit: '',
     penerbitEdit: '',
@@ -89,95 +109,115 @@ const inisialization = {
     categoryEdit: 'Umum',
     deskripsiBukuEdit: '',
     bookIdEdit: '',
+    successEditBook: true,
+
+    // Admin related
     adminUserList: [],
     adminTransaksiList: [],
-    successAddBook: true,
-    successEditBook: true,
+    
+    // Localhost for backend
     localHost: 'https://serbabuku.club/'
 }
 
-// consy localHost = 'http://serbabuku.club/'
+// const localHost = 'http://serbabuku.club/'
 // const localHost = 'http://13.229.61.125:5000/'
 // const localHost = 'http://0.0.0.0:5000/'
 const localHost = inisialization.localHost
 
+// Creating store
 export const store = createStore(inisialization)
 
 export const actions = store => (
 {
+    // Function to handle input change, used in many inputs in forms
     handleChange: (state, event) => {
         store.setState({[event.target.name]: event.target.value})
     },
 
+    // Function to handle submit form event
     handleSubmit: (state, event) => {
         event.preventDefault()
     },
 
+    // Increase the number in placeholder in the detail book page
     increaseItem: (state, event) => {
         const placeholderStartNow = state.placeholderStart + 1
         store.setState({placeholderStart: placeholderStartNow})
     },
 
+    // Decrease the number in placeholder in the detail book page
     decreaseItem: (state, event) => {
+        // To verify that the placeholder minimum value is 0
         if (state.placeholderStart >= 1){
             const placeholderStartNow = state.placeholderStart - 1
             store.setState({placeholderStart: placeholderStartNow})   
         }
     },
 
+    // Add books to active cart
     addToCart: async (state, event) => {
         await axios({method: 'patch', url: localHost + "users/buku/" + state.dataDetilBuku.idBuku, headers: {"Authorization" : "Bearer " + state.tokenLogin}, data: {jumlah_pembelian: state.placeholderStart} })
-            .then(function(response){
+            .then(response => {
                 store.setState({placeholderStart: 0})
                 alert(response.data.message)
             })
-            .catch(function(response){
+            .catch(response => {
                 alert(response.message)
             })
     },
 
+    // Showing all books in active cart
     showCart: async (state, event) => {
         await axios
             .get(localHost + "users/keranjang", {headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-            .then(function(response){
-                store.setState({cartList: response.data.data_transaksi})
-                store.setState({jumlahPembelian: response.data.jumlah_pembelian, totalHarga: response.data.total_harga, usernamePenjualCart: response.data.data_penjual.username, alamatCart: response.data.data_penjual.alamat, nomorHpCart: response.data.data_penjual.nomor_hp})
+            .then(response => {
+                store.setState({
+                    cartList: response.data.data_transaksi, 
+                    jumlahPembelian: response.data.jumlah_pembelian, 
+                    totalHarga: response.data.total_harga, 
+                    usernamePenjualCart: response.data.data_penjual.username, 
+                    alamatCart: response.data.data_penjual.alamat, 
+                    nomorHpCart: response.data.data_penjual.nomor_hp
+                })
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Send an order in active cart
     sentCart: async (state, event) => {
         await axios({method: 'put', url: localHost + "users/keranjang", headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-            .then(function(response){
+            .then(response => {
                 store.setState({cartList: []})
                 alert(response.data.message)
             })
-            .catch(function(response){
+            .catch(response => {
                 alert(response.data.message)
             })
     },
 
+    // Handling accept order event
     acceptOrder: async (state, notificationId) => {
         await axios({method: 'post', url: localHost + "users/profile", headers: {"Authorization" : "Bearer " + state.tokenLogin}, data: {transaction_id: notificationId, status: 'terima'}})
-            .then(function(response){
+            .then(response => {
                 alert("Sukses menerima pesanan!")
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Handling reject order event
     rejectOrder: async (state, notificationId) => {
         await axios({method: 'post', url: localHost + "users/profile", headers: {"Authorization" : "Bearer " + state.tokenLogin}, data: {transaction_id: notificationId, status: 'tolak'}})
-            .then(function(response){
+            .then(response => {
                 alert("Sukses menolak pesanan!")
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Send register data to flask
     handleRegister: async (state, event) => {
-        const self = this
         const data = {
             email: state.emailRegister,
             username: state.usernameRegister,
@@ -190,22 +230,31 @@ export const actions = store => (
         }
         await axios
             .post(localHost + 'register', data)
-            .then(function(response){
+            .then(response => {
                 alert(response.data.message)
                 if (response.data.message === 'Selamat! Akunmu sudah terdaftar sekarang'){
-                    store.setState({isValid: true})
-                    store.setState({emailRegister: '', passwordRegister: '', confirmPasswordRegister: '', namaLengkapRegister: '', fotoProfilRegister: '', alamatRegister: '', nomorHpRegister: ''})
+                    store.setState({
+                        isValid: true, 
+                        emailRegister: '', 
+                        passwordRegister: '', 
+                        confirmPasswordRegister: '', 
+                        namaLengkapRegister: '', 
+                        fotoProfilRegister: '', 
+                        alamatRegister: '', 
+                        nomorHpRegister: ''
+                    })
                 }
                 else {
                     store.setState({isValid: false})
                 }
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({isValid: false})
                 alert(response.data.message)
             })
     },
 
+    // Handle login event
     handleLogin: async (state, event) => {
         const data = {
             username: state.usernameLogin,
@@ -214,47 +263,52 @@ export const actions = store => (
         
         await axios
             .post(localHost + 'login', data)
-            .then(function(response){
+            .then(response => {
                 store.setState({isLogin: true, tokenLogin: response.data.token, isLoading: true})
                 alert("Sukses Login!")
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({tokenLogin: '', isLogin: false})
                 alert("Username atau password yang kamu masukkan salah")
             })
     },
 
+    // Show all users list in admin page
     handleAdmin: async (state, event) => {
         await axios({method: 'get', url: localHost + "admin/users", headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-            .then(function(response){
+            .then(response => {
                 store.setState({adminUserList: response.data})
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Show all transactions in admin page
     showAdminTransaksi: async (state, event) => {
         await axios({method: 'get', url: localHost + "admin/transaksi", headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-            .then(function(response){
+            .then(response => {
                 store.setState({adminTransaksiList: response.data})
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Handling event when admin delete an user
     deleteUser: async (state, userId) => {
         await axios({method: 'delete', url: localHost + "admin/users", headers: {"Authorization" : "Bearer " + state.tokenLogin}, data: {user_id: userId}})
-            .then(function(response){
+            .then(response => {
                 alert("Kamu telah menghapus user terkait")
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Handling logout event
     handleLogout: async (state, event) => {
         store.setState(inisialization)
     },
 
+    // Add a book in profile page
     addBook: async (state, event) => {
         const data = {
             judul_buku: state.judulBukuAdd,
@@ -268,34 +322,40 @@ export const actions = store => (
             stok: state.stokAdd
         }
         await axios({method: 'post', url: localHost + "users/profile/tambah", headers: {"Authorization" : "Bearer " + state.tokenLogin}, data: data})
-            .then(function(response){
+            .then(response => {
                 store.setState({successAddBook: true})
                 alert("Kamu sukses menambahkan buku")
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({successAddBook: false})
+                
+                // Case when there is at least one field left empty
                 if(state.judulBukuAdd === '' || state.pengarangAdd === '' || state.penerbitAdd === '' || state.deskripsiBukuAdd === '' || state.categoryAdd === '' || state.stokAdd === '' || state.hargaSatuanAdd === ''){
                     alert("Tidak boleh ada field yang dikosongkan (kecuali foto buku)")
                 }
+
+                // Case when the book has already added
                 else{
                     alert("Buku yang kamu ingin tambahkan sudah ada di daftar buku yang kamu jual saat ini")
                 }
             })
     },
 
+    // Delete a book
     deleteBook: async (state, bookId) => {
         await axios({method: 'delete', url: localHost + "users/profile/edit-buku/" + bookId, headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-            .then(function(response){
+            .then(response => {
                 alert("Kamu sukses menghapus buku")
             })
-            .catch(function(response){
+            .catch(response => {
                 alert(response.message)
             })
     },
 
+    // Prepare the form for book editting (fill the placeholder with the book informations)
     prepareEditBook: async (state, bookId) => {
         await axios({method: 'get', url: localHost + "users/profile/edit-buku/" + bookId, headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-        .then(function(response){
+        .then(response => {
             store.setState({
                 judulBukuEdit: response.data.judul_buku,
                 penerbitEdit: response.data.penerbit,
@@ -309,10 +369,11 @@ export const actions = store => (
                 bookIdEdit: bookId
             })
         })
-        .catch(function(response){
+        .catch(response => {
         })
     },
 
+    // Send the editted data to flask
     editBook: async (state, bookId) => {
         const data = {
             judul_buku: state.judulBukuEdit,
@@ -326,19 +387,20 @@ export const actions = store => (
             stok: state.stokEdit
         }
         await axios({method: 'put', url: localHost + "users/profile/edit-buku/" + bookId, headers: {"Authorization" : "Bearer " + state.tokenLogin}, data: data})        
-        .then(function(response){
+        .then(response => {
             store.setState({successEditBook: true})
             alert("Kamu sukses mengedit buku")
         })
-        .catch(function(response){
+        .catch(response => {
             store.setState({successEditBook: false})
             alert("Tidak boleh ada field yang dikosongkan (kecuali foto buku)")
         })
     },
 
+    // Show profile user data
     showProfile: async (state, event) => {
         await axios({method: 'get', url: localHost + "users/profile", headers: {"Authorization" : "Bearer " + state.tokenLogin}})
-            .then(function(response){
+            .then(response => {
                 store.setState({
                     profileUserData: {
                         email: response.data.user_data.email,
@@ -353,10 +415,11 @@ export const actions = store => (
                     historyList: response.data.history_belanja
                 })
             })
-            .catch(function(response){
+            .catch(response => {
             })
     },
 
+    // Handling seaeching event
     handleSearch: (state, event) => {
         let penjual = state.namaUserPenjual
         let penerbit = state.penerbit
@@ -365,6 +428,7 @@ export const actions = store => (
         let idBuku = state.idBuku
         let judulBuku = state.judulBuku
 
+        // Filter part
         let searchQuery = localHost + 'public/buku?'
         if (judulBuku !== ''){
             searchQuery = searchQuery + 'judul_buku=' + judulBuku + '&'
@@ -387,35 +451,37 @@ export const actions = store => (
 
         axios
             .get(searchQuery)
-            .then(function(response){
+            .then(response => {
                 store.setState({daftarBuku: response.data, isLoading: false, isSearch: true, isFilter: false})
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({isLoading: false})
             })
     },
 
+    // To make the serach and filter related state back to default
     resetSearch: (state, event) => {
         store.setState({isSearch: false, isFilter: false, judulBuku: '', namaUserPenjual: '', pengarang: '', penerbit: '', nomorIsbn: '', idBuku: '', category: 'Umum'})
     },
 
+    // Handle filter event
     handleFilter: (state, event) => {
         let searchQuery = localHost + 'public/buku?kategori=' + state.category
         axios
             .get(searchQuery)
-            .then(function(response){
+            .then(response => {
                 store.setState({daftarBuku: response.data, isLoading: false, isSearch: true, isFilter: true, activeCategory: state.category})
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({isLoading: false})
             })
     },
 
+    // Preparing for detail book page for user
     clickBookDetailUser: async (state, bookId) => {
-        const self = this
         await axios
             .get(localHost + "users/buku/" + bookId, {headers: {"Authorization" : "Bearer " + state.tokenLogin} })
-            .then(function(response){
+            .then(response => {
                 store.setState({
                     dataDetilPenjual: {
                         username: response.data.data_penjual.username,
@@ -440,16 +506,16 @@ export const actions = store => (
                     placeholderStart: response.data.placeholder_start
                 })
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({isLoading: false})
             })
         },
 
+    // Preparing for detail book page for public
     clickBookDetail: async (state, bookId) => {
-        const self = this
         await axios
             .get(localHost + "public/buku/" + bookId)
-            .then(function(response){
+            .then(response => {
                 store.setState({
                     dataDetilPenjual: {
                         username: response.data.data_penjual.username,
@@ -472,7 +538,7 @@ export const actions = store => (
                     }
                 })
             })
-            .catch(function(response){
+            .catch(response => {
                 store.setState({isLoading: false})
             })
         }
